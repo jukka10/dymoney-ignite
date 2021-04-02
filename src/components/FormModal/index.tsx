1cc2ea0841c30b9041a71fba731f6fe6ae54d3d8
jukka <jukka4996@gmail.com>
2021-04-02 07:10:55 -0300

    styling fineshed

--- src/components/FormModal/index.tsx
+++ src/components/FormModal/index.tsx
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import closeImg from "../../assets/close.svg";
-import { Container } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+
+import { Container, Radioutton, TransactionTypeContainer } from "./styles";
 
 interface FormModalProp {
   onHandleCloseModal: React.MouseEventHandler;
 }
 
 export const FormModal: React.FC<FormModalProp> = ({ onHandleCloseModal }) => {
+  const [type, setType] = useState<"deposit" | "withdraw">("deposit");
+
   return (
     <Container>
       <h2>NovaTransação</h2>
 
       <input placeholder="Titulo" />
       <input placeholder="Valor" type="number" />
+
+      <TransactionTypeContainer>
+        <Radioutton
+          type="button"
+          onClick={() => setType("deposit")}
+          isActive={type === "deposit"}
+          activeColor="green"
+        >
+          <img src={incomeImg} alt="Entrada" />
+          <span>Entrada</span>
+        </Radioutton>
+
+        <Radioutton
+          type="button"
+          onClick={() => setType("withdraw")}
+          isActive={type === "withdraw"}
+          activeColor="red"
+        >
+          <img src={outcomeImg} alt="Saída" />
+          <span>Saída</span>
+        </Radioutton>
+      </TransactionTypeContainer>
+
       <input placeholder="Categoria" />
 
       <button type="submit">Cadastrar</button>
--- src/components/FormModal/styles.ts
+++ src/components/FormModal/styles.ts
@@ -1,4 +1,15 @@
+import { darken, transparentize } from "polished";
 import styled from "styled-components";
+
+interface RadioButtonProp {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
+
+const colors = {
+  green: "#12A454",
+  red: "#E52E4D",
+};
 
 export const Container = styled.form`
   h2 {
@@ -43,5 +54,43 @@ export const Container = styled.form`
     &:hover {
       filter: brightness(0.9);
     }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+`;
+
+export const Radioutton = styled.button<RadioButtonProp>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: ${({ isActive, activeColor }) =>
+    isActive ? transparentize(0.8, colors[activeColor]) : "transparent"};
+
+  transition: filter 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.9, "#d7d7d7")};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--title);
   }
 `;
